52bf0b0dfd3fba605f41e47e26b1d26ec5dc471d
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-03 14:19:47 +0200

    added basic state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  // write super when writing constructer
+  super();
+
+  // always json object
+  this.state = {
+name:'JOHN'
+  }
+}
+
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            HELLO WRLD, this is JOHN
+            HELLO WRLD, this is {this.state.name}
           </p>
          <button>Change Name</button>
         </header>
