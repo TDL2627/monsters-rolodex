ca4952cf309b1c737c7de10ad2294a9c9dcf5018
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-03 15:09:05 +0200

    started with the monster app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,15 @@ constructor(){
 
   // always json object
   this.state = {
-name:{firstName:'John',lastName:'Wick'},
-age:'200'
+monster1:{
+  name:'Mike'
+},
+monster2:{
+  name:'Dracula'
+},
+monster3:{
+  name:'Venom'
+}
   };
 }
 
@@ -19,36 +26,10 @@ age:'200'
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            HELLO WRLD, this is {this.state.name.firstName}  {this.state.name.lastName}  , I am {this.state.age} years old.
-          </p>
-          {/* changing state via function */}
-          <button 
-          onClick={()=>{
-            this.setState((state, props) => {
-              return {
-                name:{firstName:'Peter',lastName:'Pan'},
-              }
-            },()=>{
-              console.log(this.state)
-            });
-          }}
-          
-          >
-Change state via function
-          </button>
-          <br></br>
-         <button
-         onClick={ ()=>{
-            //  Changing state via object
-             this.setState({name:{firstName:'Peter',lastName:'Pan'}})
-             this.setState({age:'15'});
-            //  console.log(this.state)
-           }}
-         >Change Name via object</button>
-        </header>
+      <h1>{this.state.monster1.name}</h1>
+      <h1>{this.state.monster2.name}</h1>
+      <h1>{this.state.monster3.name}</h1>
+
       </div>
     );
   }
