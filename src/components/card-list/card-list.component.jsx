256776320a3ef3b84a3dbb07897f67e5cf9653a7
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-04 12:19:14 +0200

    added basic styling

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,14 +3,16 @@ import { Component } from "react";
 class CardList extends Component {
     render(){
         const {monsters} = this.props;
-        return<div>
-            {
-                monsters.map((monster)=>(
-                    <h1 key={monster.id}>{monster.name}</h1>
-
-                ))
-            }
+        return(  <div className="card-list">
+            {monsters.map((monster)=>(
+                <div key={monster.id} className="card-container">
+                    <img src={`https://robohash.org/${monster.id}?set=set2`} alt={`monster ${monster.name}`} />
+                    <h2 >{monster.name}</h2>
+                    <p>{monster.email}</p>
+</div>
+                ))}
             </div>
+        )
     }
 }
 export default CardList;
