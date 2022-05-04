125a3bed1e690c562fcf026c4559c808d1547218
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-04 11:03:53 +0200

    adding components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -44,10 +45,7 @@ onSearchChange=(event)=>{
         {/* search function */}
 
 <input className='search-box' type='search' placeholder='Search monsters' onChange={onSearchChange}/>
-        {filteredMonsters.map((monster)=>{
-  return <div key={monster.id}><h1>{monster.name}</h1></div>
-          })}
-     
+     <CardList monsters={filteredMonsters}/>
 
       </div>
     );
